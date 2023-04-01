7cf6c5e20faf6df182ba81da458ff6114acd4ed0
KashyapPLN <kashyap.pln@gmail.com>
2023-04-01 11:30:31 +0530

    container added

--- src/paripoorna task/TableTask.js
+++ src/paripoorna task/TableTask.js
@@ -3,6 +3,7 @@ import Screen1 from './Screen1'
 
 import { useState } from 'react';
 import SortingTableComponent from './Screen2';
+import { Container } from 'react-bootstrap';
 export default function TableTask() {
     const [disp1,setDisp1]=useState(false);
     const [disp2,setDisp2]=useState(false);
@@ -12,8 +13,10 @@ export default function TableTask() {
         <button onClick={(e)=>{setDisp1(true);setDisp2(false);}}style={{margin:'40px'}}>Screen1</button>
         <button onClick={(e)=>{setDisp2(true);setDisp1(false);}}>Screen2</button>
         </div>
+        <Container style={{border: 'solid',padding:'20px'}}>
         {disp1===true ? <Screen1/>: null}
         {disp2===true ? <SortingTableComponent/>: null}
+        </Container>
     </div>
   )
 }
